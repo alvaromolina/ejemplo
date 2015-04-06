ba1c948f4d37468e4d370ef67235e6f1edba9528
Alvaro Molina} <alvaromolinac@gmail.com>
2015-04-06 13:47:58 -0400

    nuevas columnas books

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406173530) do
+ActiveRecord::Schema.define(version: 20150406174358) do
 
   create_table "books", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "author"
+    t.text     "rating"
   end
 
 end
